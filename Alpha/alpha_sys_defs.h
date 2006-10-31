5c173cdd67c35bc18b886969659812657179b9e4
Bob Supnik <bob@supnik.org>
2006-10-31 15:17:00 -0700

    simh Alpha CPU

---
+++ Alpha/alpha_sys_defs.h
@@ -0,0 +1,43 @@
+/* alpha_system_defs.h: Alpha system definitions file
+
+   Copyright (c) 2003-2006, Robert M Supnik
+
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+   ROBERT M SUPNIK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+   IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+   Except as contained in this notice, the name of Robert M Supnik shall not be
+   used in advertising or otherwise to promote the sale, use or other dealings
+   in this Software without prior written authorization from Robert M Supnik.
+
+   Respectfully dedicated to the great people of the Alpha chip, systems, and
+   software development projects; and to the memory of Peter Conklin, of the
+   Alpha Program Office.
+
+   This is a STUB!
+*/
+
+#ifndef _ALPHA_SYS_DEFS_H_
+#define _ALPHA_SYS_DEFS_H_      0
+
+#define PA_SIZE                 36                       /* PA size */
+#define PA_MASK                 0x0000000FFFFFFFFF
+
+#define ROMBASE                 0x000000FFFC000000
+#define ROMSIZE                 0x0000000004000000
+
+#endif
+
