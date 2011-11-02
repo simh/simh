158a02df5a29c55f890c3befa56fce421f6d6ac4
Mark Pizzolato <mark@infocomm.com>
2011-11-02 08:45:21 -0700

    Removed unused utility routine which doesn't compile on some platforms

--- VAX/vax780_stddev.c
+++ VAX/vax780_stddev.c
@@ -733,32 +733,6 @@ todr_resync ();                                         /* resync TODR */
 return SCPE_OK;
 }
 
-
-int
-timeval_subtract (result, x, y)
-  struct timeval *result, *x, *y;
-{
-/* Perform the carry for the later subtraction by updating y. */
-if (x->tv_usec < y->tv_usec) {
- int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
- y->tv_usec -= 1000000 * nsec;
- y->tv_sec += nsec;
-}
-if (x->tv_usec - y->tv_usec > 1000000) {
- int nsec = (x->tv_usec - y->tv_usec) / 1000000;
- y->tv_usec += 1000000 * nsec;
- y->tv_sec -= nsec;
-}
-
-/* Compute the time remaining to wait.
-  tv_usec is certainly positive. */
-result->tv_sec = x->tv_sec - y->tv_sec;
-result->tv_usec = x->tv_usec - y->tv_usec;
-
-/* Return 1 if result is negative. */
-return x->tv_sec < y->tv_sec;
-}
-
 /* TODR routines */
 
 int32 todr_rd (void)
