cbc14bc01012d8aa24a0b7519b91a68aa609fa9f
Mark Pizzolato <mark@infocomm.com>
2011-11-02 05:45:01 -0700

    Fix memory leak on error path and proper polling vs async conditions

--- PDP11/pdp11_xq.h
+++ PDP11/pdp11_xq.h
@@ -87,7 +87,7 @@ extern int32 int_req[IPL_HLVL];
 
 #define XQ_QUE_MAX           500                        /* read queue size in packets */
 #define XQ_FILTER_MAX         14                        /* number of filters allowed */
-#if defined SIM_ASYNCH_IO
+#if defined(SIM_ASYNCH_IO) && defined(USE_READER_THREAD)
 #define XQ_SERVICE_INTERVAL  0                          /* polling interval - No Polling with Asynch I/O */
 #else
 #define XQ_SERVICE_INTERVAL  100                        /* polling interval - X per second */
