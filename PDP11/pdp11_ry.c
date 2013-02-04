8a97baa553e2231c08719d108ba48ab754b80a80
Mark Pizzolato <mark@infocomm.com>
2013-02-04 04:29:39 -0800

    Added MTAB entry descriptions

--- PDP11/pdp11_ry.c
+++ PDP11/pdp11_ry.c
@@ -227,11 +227,11 @@ MTAB ry_mod[] = {
     { (UNIT_AUTO+UNIT_DEN+UNIT_ATT),  UNIT_DEN, "double density",   NULL, NULL },
     { (UNIT_AUTO+UNIT_ATT),          UNIT_AUTO, "autosize",         NULL, NULL },
     { UNIT_AUTO,                     UNIT_AUTO, NULL,               "AUTOSIZE", 
-        NULL },
+        NULL, NULL, NULL, "set density based on file size at ATTACH" },
     { (UNIT_AUTO+UNIT_DEN),                  0, NULL,               "SINGLE", 
-        &ry_set_size },
+        &ry_set_size, NULL, NULL, "Set to Single density (256Kb)" },
     { (UNIT_AUTO+UNIT_DEN),           UNIT_DEN, NULL,               "DOUBLE", 
-        &ry_set_size },
+        &ry_set_size, NULL, NULL, "Set to Double density (512Kb)" },
 #if defined (VM_PDP11)
     { MTAB_XTD|MTAB_VDV|MTAB_VALR, 004, "ADDRESS", "ADDRESS",
       &set_addr, &show_addr, NULL, "Bus Address" },
@@ -240,10 +240,10 @@ MTAB ry_mod[] = {
     { MTAB_XTD|MTAB_VDV|MTAB_VALR, 0, "VECTOR", "VECTOR",
       &set_vec, &show_vec, NULL, "Interrupt vector" },
 #else
-    { MTAB_XTD|MTAB_VDV, 004, "ADDRESS", "ADDRESS",
-      NULL, &show_addr, NULL, "Bus Address" },
-    { MTAB_XTD|MTAB_VDV, 0, "VECTOR", "VECTOR",
-      NULL, &show_vec, NULL, "Interrupt vector" },
+    { MTAB_XTD|MTAB_VDV, 004, NULL, "ADDRESS",
+      NULL, &show_addr, NULL, "Display Bus Address" },
+    { MTAB_XTD|MTAB_VDV, 0, NULL, "VECTOR",
+      NULL, &show_vec, NULL, "Display Interrupt vector" },
 #endif
     { 0 }
     };
