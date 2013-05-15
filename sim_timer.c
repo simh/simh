c097ae5e572fc9d2a4895546e60f761dab6e1230
Mark Pizzolato <mark@infocomm.com>
2013-05-15 05:43:14 -0700

    Added help info to asynchronous timer device

--- sim_timer.c
+++ sim_timer.c
@@ -795,8 +795,8 @@ return SCPE_OK;
 
 MTAB sim_timer_mod[] = {
 #if defined (SIM_ASYNCH_IO) && defined (SIM_ASYNCH_CLOCKS)
-  { MTAB_XTD|MTAB_VDV, 0, "ASYNC", "ASYNC", &sim_timer_set_async, &sim_timer_show_async },
-  { MTAB_XTD|MTAB_VDV, 0, NULL, "NOASYNC", &sim_timer_clr_async, NULL },
+  { MTAB_VDV,          MTAB_VDV, "ASYNC", "ASYNC",   &sim_timer_set_async, &sim_timer_show_async, NULL, "Enables/Displays Asynchronous Timer operation mode" },
+  { MTAB_VDV,                 0,    NULL, "NOASYNC", &sim_timer_clr_async, NULL,                  NULL, "Disables Asynchronous Timer operation" },
 #endif
   { 0 },
 };
