6de3662eaaf72211423381610e876271c7f73b32
Timothe Litt <tlhackque@yahoo.com>
2013-07-11 16:28:42 -0400

    Fix sim_devices entries for DMCs
    Still used old arrayed device, causing autoconfigure crashes.

--- VAX/vax730_syslist.c
+++ VAX/vax730_syslist.c
@@ -52,7 +52,7 @@ extern DEVICE tq_dev;
 extern DEVICE dz_dev;
 extern DEVICE vh_dev;
 extern DEVICE xu_dev, xub_dev;
-extern DEVICE dmc_dev[];
+extern DEVICE dmc_dev;
 extern DEVICE dup_dev;
 
 extern UNIT cpu_unit;
@@ -85,10 +85,7 @@ DEVICE *sim_devices[] = {
     &tq_dev,
     &xu_dev,
     &xub_dev,
-    &dmc_dev[0],
-    &dmc_dev[1],
-    &dmc_dev[2],
-    &dmc_dev[3],
+    &dmc_dev,
     &dup_dev,
     NULL
     };
--- VAX/vax780_syslist.c
+++ VAX/vax780_syslist.c
@@ -54,7 +54,7 @@ extern DEVICE tu_dev;
 extern DEVICE dz_dev;
 extern DEVICE vh_dev;
 extern DEVICE xu_dev, xub_dev;
-extern DEVICE dmc_dev[];
+extern DEVICE dmc_dev;
 extern DEVICE dup_dev;
 
 extern UNIT cpu_unit;
@@ -92,10 +92,7 @@ DEVICE *sim_devices[] = {
     &tq_dev,
     &xu_dev,
     &xub_dev,
-    &dmc_dev[0],
-    &dmc_dev[1],
-    &dmc_dev[2],
-    &dmc_dev[3],
+    &dmc_dev,
     &dup_dev,
     NULL
     };
