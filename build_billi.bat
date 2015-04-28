839103b292267c0d65464f7c0831540ebd8ef768
Bill Beech <nj7p@nj7p.org>
2015-04-28 14:47:32 -0700

    AL: Remove inappropriate file additions

--- build_billi.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem Build Intel Systems
-call build_mingw.bat isys8010
-pause
-
--- build_billm.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem Build swtp6800 systems
-call build_mingw.bat swtp6800mp-a swtp6800mp-a2
-pause
-
