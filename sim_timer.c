094cbf829597e069d4daf0c3862653c1ebf026eb
Mark Pizzolato <mark@infocomm.com>
2015-12-17 06:44:03 -0800

    TIMER: Avoiding conditional directives that break statements
    Based on Romero B. de S. Malaquias comments Fixes #256

--- sim_timer.c
+++ sim_timer.c
@@ -922,13 +922,9 @@ int32 act_cyc;
 
 if ((!sim_idle_enab)                             ||     /* idling disabled */
     ((sim_clock_queue == QUEUE_LIST_END) &&             /* or clock queue empty? */
-#if defined(SIM_ASYNCH_IO) && defined(SIM_ASYNCH_CLOCKS)
      (!(sim_asynch_enabled && sim_asynch_timer)))||     /*     and not asynch? */
-#else
-     (TRUE))                                     ||
-#endif
-    ((sim_clock_queue != QUEUE_LIST_END) && 
-     ((sim_clock_queue->flags & UNIT_IDLE) == 0))||     /* or event not idle-able? */
+    ((sim_clock_queue != QUEUE_LIST_END) &&             /* or clock queue not empty */
+     ((sim_clock_queue->flags & UNIT_IDLE) == 0))||     /*   and event not idle-able? */
     (rtc_elapsed[tmr] < sim_idle_stable)) {             /* or timer not stable? */
     if (sin_cyc)
         sim_interval = sim_interval - 1;
