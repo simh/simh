688416a4d7de66a7f78dcd70b59347af9bef12d4
Mark Pizzolato <mark@infocomm.com>
2016-01-21 07:37:06 -0800

    VAX, MicroVAX1, MicroVAX2: Provide consistent messages when attempting to change CPU Model.
    As discussed in issue #269

--- VAX/vax_sysdev.c
+++ VAX/vax_sysdev.c
@@ -1845,7 +1845,7 @@ else if (MATCH_CMD(gbuf, "VAXSTATION") == 0) {
     vs_dev.flags = vs_dev.flags & ~DEV_DIS;              /* enable mouse */
     reset_all (0);                                       /* reset everything */
 #else
-    return SCPE_ARG;
+    return sim_messagef(SCPE_ARG, "Simulator built without Graphic Device Support");
 #endif
     }
 else
