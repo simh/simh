a008b0a9726dc1400e85a3714af7bfc8583c444a
Mark Pizzolato <mark@infocomm.com>
2016-09-14 14:12:24 -0700

    SCP: Fix throttling to provide reasonable operation of calibrated clocks
    - Generate reasonable messages when presented with erroneous throttle input.
    - Add throttling recalibration logic if only if target rate drift exceeds 5%

--- sim_timer.h
+++ sim_timer.h
@@ -85,16 +85,17 @@ int clock_gettime(int clock_id, struct timespec *tp);
 #define SIM_IDLE_STDFLT 20                          /* dft sec for stability */
 #define SIM_IDLE_STMAX  600                         /* max sec for stability */
 
-#define SIM_THROT_WINIT 1000                        /* cycles to skip */
-#define SIM_THROT_WST   10000                       /* initial wait */
-#define SIM_THROT_WMUL  4                           /* multiplier */
-#define SIM_THROT_WMIN  100                         /* min wait */
-#define SIM_THROT_MSMIN 10                          /* min for measurement */
-#define SIM_THROT_NONE  0                           /* throttle parameters */
-#define SIM_THROT_MCYC  1                           /* MegaCycles Per Sec */
-#define SIM_THROT_KCYC  2                           /* KiloCycles Per Sec */
-#define SIM_THROT_PCT   3                           /* Max Percent of host CPU */
-#define SIM_THROT_SPC   4                           /* Specific periodic Delay */
+#define SIM_THROT_WINIT     1000                    /* cycles to skip */
+#define SIM_THROT_WST       10000                   /* initial wait */
+#define SIM_THROT_WMUL      4                       /* multiplier */
+#define SIM_THROT_WMIN      100                     /* min wait */
+#define SIM_THROT_DRIFT_PCT 5                       /* drift percentage for recalibrate */
+#define SIM_THROT_MSMIN     10                      /* min for measurement */
+#define SIM_THROT_NONE      0                       /* throttle parameters */
+#define SIM_THROT_MCYC      1                       /* MegaCycles Per Sec */
+#define SIM_THROT_KCYC      2                       /* KiloCycles Per Sec */
+#define SIM_THROT_PCT       3                       /* Max Percent of host CPU */
+#define SIM_THROT_SPC       4                       /* Specific periodic Delay */
 
 #define TIMER_DBG_IDLE  0x001                       /* Debug Flag for Idle Debugging */
 #define TIMER_DBG_QUEUE 0x002                       /* Debug Flag for Asynch Queue Debugging */
