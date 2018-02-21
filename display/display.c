62678623b8da54cf7f1a5163da6cef92b9bdd3bf
Phil Budne <phil@ultimate.com>
2018-02-20 23:48:46 -0500

    DISPLAY: remove duplicate end of block comment oops

--- display/display.c
+++ display/display.c
@@ -113,7 +113,6 @@ struct display {
  * https://www.youtube.com/watch?v=hZumwXS4fJo
  * "3RP7A CRT - P7 Phosphor Persistence" shows colors/persistence
  */
- */
 static struct phosphor p7[] = {
     {0.11, 0.11, 1.0,  0.5, 0.05},  /* fast blue */
     {1.0,  1.0,  0.11, 0.5, 0.20}   /* slow yellow/green */
