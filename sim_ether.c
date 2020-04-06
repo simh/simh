07c274ac9783752d2aa2ee3497aceccf38fb51e9
Mark Pizzolato <mark@infocomm.com>
2020-04-06 10:36:31 -0700

    ETHER: Restore support fir Windows Npcap as Ethernet packet API
    Version of Npcap 0.9990 restores full WinPcap functionality
    
    As discussed in nmap/nmap#1929 and nmap/nmap#1343

--- sim_ether.c
+++ sim_ether.c
@@ -2478,10 +2478,17 @@ const char *eth_version (void)
 static char version[256];
 
 if (!version[0]) {
-  if (memcmp(pcap_lib_version(), "Npcap", 5))
-    strlcpy(version, pcap_lib_version(), sizeof(version));
-  else
-    snprintf(version, sizeof(version), "Unsupported - %s", pcap_lib_version());
+  strlcpy(version, pcap_lib_version(), sizeof(version));
+  if (memcmp(pcap_lib_version(), "Npcap", 5) == 0) {
+    char maj_min[CBUFSIZE];
+    char *c = version;
+
+    while (*c && !isdigit (*c))
+      ++c;
+    get_glyph (c, maj_min, ',');
+    if (strcmp ("0.9990", maj_min) < 0)
+      snprintf(version, sizeof(version), "Unsupported - %s", pcap_lib_version());
+    }
   }
 return version;
 #else
