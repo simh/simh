482e6b2ff95e563d3649f961bb3972737b54b9a4
Patrick Linstruth <patrick@deltecent.com>
2020-06-22 09:08:34 -0700

    AltairZ80: adds MITS 88-2SIO "M2SIO0" and "M2SIO1" devices
    This commit adds two new devices, "M2SIO0" and "M2SIO1", to the AltairZ80
    VM. These two independent devices emulate "raw" 88-2SIO ports that can
    be used instead of the "SIO" device for attaching socket and serial
    ports using TMXR.

--- AltairZ80/altairz80_sys.c
+++ AltairZ80/altairz80_sys.c
@@ -68,6 +68,8 @@ extern DEVICE hdc1001_dev;
 
 extern DEVICE jade_dev;
 extern DEVICE tarbell_dev;
+extern DEVICE m2sio0_dev;
+extern DEVICE m2sio1_dev;
 
 extern DEVICE cromfdc_dev;
 extern DEVICE wd179x_dev;
@@ -123,6 +125,9 @@ DEVICE      *sim_devices[]  = {
     &jade_dev,
     /* Tarbell Devices */
     &tarbell_dev,
+    /* MITS 88-2SIO */
+    &m2sio0_dev,
+    &m2sio1_dev,
     /* Vector Graphic Devices */
     &fw2_dev, &vfdhd_dev,
     /* Single-Board Computers */
