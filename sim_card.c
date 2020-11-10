47dbc558a2680c70444e291dfce2e39f00271ef4
Richard Cornwell <rich@sky-visions.com>
2020-11-09 19:32:36 -0500

    CARD: Updated translation table.

--- sim_card.c
+++ sim_card.c
@@ -153,7 +153,7 @@ static const uint16          ascii_to_hol_026[128] = {
    /*   p      q      r      s      t      u      v      w */
     0xC04, 0xC02, 0xC01, 0x680, 0x640, 0x620, 0x610, 0x608,
    /*   x      y      z      {      |      }      ~    del */
-   /*                     T79     Y78   Y79     79         */
+   /*                     T79     X78   Y79     79         */
     0x604, 0x602, 0x601, 0x406, 0x806, 0x805, 0x005, 0xf000
 };
 
@@ -189,17 +189,17 @@ static const uint16          ascii_to_hol_029[128] = {
    /* Y7     Y8     Y9     T2     T3     T4     T5     T6  */
     0x404, 0x402, 0x401, 0x280, 0x240, 0x220, 0x210, 0x208,
    /*   X      Y      Z      [      \      ]      ^      _ */
-   /* T7     T8     T9     X0     T28    Y28    T78    T58 */
-    0x204, 0x202, 0x201, 0xA00, 0x282, 0x882, 0x406, 0x212,
+   /* T7     T8     T9   TY028    T28  TY038    Y78    T58 */
+    0x204, 0x202, 0x201, 0xE82, 0x282, 0xE42, 0x406, 0x212,
    /*   `      a      b      c      d      e      f      g */
-    0xf000,0xB00, 0xA80, 0xA40, 0xA20, 0xA10, 0xA08, 0xA04,     /* 140 - 177 */
+    0x102 ,0xB00, 0xA80, 0xA40, 0xA20, 0xA10, 0xA08, 0xA04,     /* 140 - 177 */
    /*   h      i      j      k      l      m      n      o */
     0xA02, 0xA01, 0xD00, 0xC80, 0xC40, 0xC20, 0xC10, 0xC08,
    /*   p      q      r      s      t      u      v      w */
     0xC04, 0xC02, 0xC01, 0x680, 0x640, 0x620, 0x610, 0x608,
    /*   x      y      z      {      |      }      ~    del */
-   /*                     Y78     YT     78    X78         */
-    0x604, 0x602, 0x601, 0x405, 0x600, 0x805, 0x806,0xf000
+   /*                      Y78    X78    X79  XTY18        */
+    0x604, 0x602, 0x601, 0x406, 0x806, 0x805, 0xF02,0xf000
 };
 
 static const uint16          ascii_to_hol_ebcdic[128] = {
@@ -233,17 +233,17 @@ static const uint16          ascii_to_hol_ebcdic[128] = {
    /* Y7     Y8     Y9     T2     T3     T4     T5     T6  */
     0x404, 0x402, 0x401, 0x280, 0x240, 0x220, 0x210, 0x208,
    /*   X      Y      Z      [      \      ]      ^      _ */
-   /* T7     T8     T9     X28    X68    T28    T78    X58 */
+   /* T7     T8     T9     X28    X68    Y28    Y78    X58 */
     0x204, 0x202, 0x201, 0x882, 0x20A, 0x482, 0x406, 0x212,
    /*   `      a      b      c      d      e      f      g */
-    0x212, 0xB00, 0xA80, 0xA40, 0xA20, 0xA10, 0xA08, 0xA04,     /* 140 - 177 */
+    0x102, 0xB00, 0xA80, 0xA40, 0xA20, 0xA10, 0xA08, 0xA04,     /* 140 - 177 */
    /*   h      i      j      k      l      m      n      o */
     0xA02, 0xA01, 0xD00, 0xC80, 0xC40, 0xC20, 0xC10, 0xC08,
    /*   p      q      r      s      t      u      v      w */
     0xC04, 0xC02, 0xC01, 0x680, 0x640, 0x620, 0x610, 0x608,
    /*   x      y      z      {      |      }      ~    del */
-   /*                     Y78     X78    78     79         */
-    0x604, 0x602, 0x601, 0x406, 0x806,0x0006,0x0005,0xf000
+   /*                     X18     X78    Y18  XYT18        */
+    0x604, 0x602, 0x601, 0x902, 0x806, 0x502, 0xF02,0xf000
 };
 
 const char          sim_ascii_to_six[128] = {
@@ -282,11 +282,11 @@ const char          sim_ascii_to_six[128] = {
 };
 
 static uint16 ebcdic_to_hol[256] = {
- /*  T0918   T91    T92    T93    T94    T95    T96   T97   0x0x */
+ /*  T918    T91    T92    T93    T94    T95    T96   T97   0x0x */
    0xB03,  0x901, 0x881, 0x841, 0x821, 0x811, 0x809, 0x805,
- /*  T98,    T918 , T928,  T938,  T948,  T958,  T968, T978   */
+ /*  T98,   T189 , T289,  T389,  T489,  T589,  T689, T789   */
    0x803,  0x903, 0x883, 0x843, 0x823, 0x813, 0x80B, 0x807,
- /*  TE918   E91    E92    E93    E94    E95    E96   E97   0x1x */
+ /* TE189    E91    E92    E93    E94    E95    E96   E97   0x1x */
    0xD03,  0x501, 0x481, 0x441, 0x421, 0x411, 0x409, 0x405,
  /*  E98     E918   E928   E938   E948   E958   E968  E978   */
    0x403,  0x503, 0x483, 0x443, 0x423, 0x413, 0x40B, 0x407,
@@ -296,15 +296,15 @@ static uint16 ebcdic_to_hol[256] = {
    0x203,  0x303, 0x283, 0x243, 0x223, 0x213, 0x20B, 0x207,
  /* TE0918   91    92     93      94     95     96     97   0x3x */
    0xF03,  0x101, 0x081, 0x041, 0x021, 0x011, 0x009, 0x005,
- /*  98      918   928    938     948    958   968     978   */
+ /*  98      189    289    389    489    589    689    789   */
    0x003,  0x103, 0x083, 0x043, 0x023, 0x013, 0x00B, 0x007,
  /*          T091  T092   T093   T094   T095   T096    T097  0x4x */
    0x000,  0xB01, 0xA81, 0xA41, 0xA21, 0xA11, 0xA09, 0xA05,
- /* T098     T18   T28    T38    T48    T58    T68     T78    */
+ /* T098     T18    T28    T38    T48    T58    T68    T78    */
    0xA03,  0x902, 0x882, 0x842, 0x822, 0x812, 0x80A, 0x806,
  /* T        TE91  TE92   TE93   TE94   TE95   TE96    TE97  0x5x */
    0x800,  0xD01, 0xC81, 0xC41, 0xC21, 0xC11, 0xC09, 0xC05,
- /* TE98     E18   E28    E38    E48    E58    E68     E78    */
+ /* TE98     E18    E28    E38    E48    E58    E68    E78   */
    0xC03,  0x502, 0x482, 0x442, 0x422, 0x412, 0x40A, 0x406,
  /* E        01    E092   E093   E094   E095   E096    E097  0x6x */
    0x400,  0x300, 0x681, 0x641, 0x621, 0x611, 0x609, 0x605,
@@ -330,7 +330,7 @@ static uint16 ebcdic_to_hol[256] = {
    0xF02,  0xF00, 0xE80, 0xE40, 0xE20, 0xE10, 0xE08, 0xE04,
  /* TE08     TE09   TE028  TE038  TE048  TE058  TE068  TE078  */
    0xE02,  0xE01, 0xE82, 0xE42, 0xE22, 0xE12, 0xE0A, 0xE06,
- /* T0       T1     T2     T3     T4     T5     T6     T7    0xcx */
+ /*  T0      T1     T2     T3     T4     T5     T6     T7    0xcx */
    0xA00,  0x900, 0x880, 0x840, 0x820, 0x810, 0x808, 0x804,
  /* T8       T9     T0928  T0938  T0948  T0958  T0968  T0978  */
    0x802,  0x801, 0xA83, 0xA43, 0xA23, 0xA13, 0xA0B, 0xA07,
@@ -339,7 +339,7 @@ static uint16 ebcdic_to_hol[256] = {
  /* E8       E9     TE928  TE938  TE948  TE958  TE968  TE978  */
    0x402,  0x401, 0xC83, 0xC43, 0xC23, 0xC13, 0xC0B, 0xC07,
  /* 028      E091   02     03     04     05     06     07    0xex  */
-   0x182,  0x701, 0x280, 0x240, 0x220, 0x210, 0x208, 0x204,
+   0x282,  0x701, 0x280, 0x240, 0x220, 0x210, 0x208, 0x204,
  /* 08       09     E0928  E0938  E0948  E0958  E0968  E0978  */
    0x202,  0x201, 0x683, 0x643, 0x623, 0x613, 0x60B, 0x607,
  /* 0        1      2      3      4      5      6      7     0xfx */
@@ -630,7 +630,7 @@ static int _cmpcard(const uint8 *p, const char *s) {
 
 t_stat
 _sim_parse_card(UNIT *uptr, DEVICE *dptr, struct _card_buffer *buf, uint16 (*image)[80]) {
-    int                   mode;
+    unsigned int          mode;
     uint16                temp;
     int                   i;
     char                  c;
@@ -1192,11 +1192,11 @@ sim_card_attach(UNIT * uptr, CONST char *cptr)
     int                  eof = 0;
     struct card_context *data;
     char                 gbuf[30];
-    int                  i;
+    unsigned int         i;
     char                *saved_filename;
-    t_bool              was_attached = (uptr->flags & UNIT_ATT);
-    t_addr              saved_pos;
-    static int          ebcdic_init = 0;
+    t_bool               was_attached = (uptr->flags & UNIT_ATT);
+    t_addr               saved_pos;
+    static int           ebcdic_init = 0;
 
     if ((uptr->flags & UNIT_RO) &&      /* Attaching a Reader */
             strchr (cptr, ',')) {       /* Restoring Attach list of files? */
@@ -1312,7 +1312,7 @@ sim_card_attach(UNIT * uptr, CONST char *cptr)
         }
         if (r == SCPE_OK) {
             const char    *fmt = "AUTO";
-            int            mode = uptr->flags & UNIT_CARD_MODE;
+            unsigned int   mode = uptr->flags & UNIT_CARD_MODE;
             for (i = 0; fmts[i].name != 0; i++) {
                 if (fmts[i].mode == mode) {
                     fmt = fmts[i].name;
