983b3ae278ded843c5895c56889316adcd4202d2
Richard Cornwell <rich@sky-visions.com>
2020-11-19 21:20:30 -0500

    KA10: Better fix to RH10/20 for Tops20.

--- PDP10/kx10_rh.c
+++ PDP10/kx10_rh.c
@@ -183,7 +183,7 @@ t_stat rh_devio(uint32 dev, uint64 *data) {
      DEVICE        *dptr = NULL;
      struct rh_if  *rhc = NULL;
      int            drive;
-     uint32         drdat;
+     uint32         drdat = 0;
 
      for (drive = 0; rh[drive].dev_num != 0; drive++) {
         if (rh[drive].dev_num == (dev & 0774)) {
@@ -243,7 +243,6 @@ t_stat rh_devio(uint32 dev, uint64 *data) {
               }
               if (rhc->reg < 040) {
                   int parity;
-                  drdat = 0;
                   if (rhc->dev_read(dptr, rhc, rhc->reg, &drdat))
                       rhc->status |= RH20_DR_RESP;
                   *data = (uint64)(drdat & 0177777);
@@ -402,7 +401,6 @@ t_stat rh_devio(uint32 dev, uint64 *data) {
                 *data = (uint64)(rhc->rae);
         } else if ((rhc->reg & 040) == 0) {
               int parity;
-              drdat = 0;
               if (rhc->dev_read(dptr, rhc, rhc->reg, &drdat)) {
                   rhc->rae |= 1 << rhc->drive;
                   rhc->status |= CR_DRE;
