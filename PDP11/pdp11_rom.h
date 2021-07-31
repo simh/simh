35a96377d787426306ead50cdaa2479f92288c1a
Jos Fries <jos.fries@kpnmail.nl>
2021-07-31 12:25:32 +0200

    Merged "HELP ATTACH" texts with hierarchical help text.

--- PDP11/pdp11_rom.h
+++ PDP11/pdp11_rom.h
@@ -80,18 +80,19 @@ SOCKET_DEF;
  */
 typedef struct
 {
-    const char *name;                                               /* Module name */
-    const uint32 valid_cpu_types;                                   /* Valid CPU types */
-    const uint32 valid_cpu_opts;                                    /* Required CPU options */
-    const uint32 num_sockets;                                       /* Number of sockets for the module type */
-    SOCKET_DEF (*sockets)[];                                        /* Sockets for this module */
-    t_stat (*auto_config)();                                        /* Auto-configuration function for the module type */
-    t_stat (*attach)(const char *);                                 /* Attach function for the module type */
-    t_stat (*auto_attach)();                                        /* Auto attach on module type selection */
-    void (*create_filename)(char *);                                /* Function to create unit file name */
-    t_stat (*read)(int32*, int32, int32);                           /* ROM read function */
-    t_stat (*set_start_address)(UNIT*, int32, CONST char*, void*);  /* Set start address function */
-    t_stat (*show_start_address)(FILE*);                            /* Show start address function */
+    const char *name;                                                   /* Module name */
+    const uint32 valid_cpu_types;                                       /* Valid CPU types */
+    const uint32 valid_cpu_opts;                                        /* Required CPU options */
+    const uint32 num_sockets;                                           /* Number of sockets for the module type */
+    SOCKET_DEF (*sockets)[];                                            /* Sockets for this module */
+    t_stat (*auto_config)();                                            /* Auto-configuration function for the module type */
+    t_stat (*attach)(const char *);                                     /* Attach function for the module type */
+    t_stat (*auto_attach)();                                            /* Auto attach on module type selection */
+    void (*create_filename)(char *);                                    /* Function to create unit file name */
+    t_stat (*read)(int32*, int32, int32);                               /* ROM read function */
+    t_stat (*set_start_address)(UNIT*, int32, CONST char*, void*);      /* Set start address function */
+    t_stat (*show_start_address)(FILE*);                                /* Show start address function */
+    t_stat (*help_attach)(FILE*, DEVICE*, UNIT*, int32, const char*);   /* Help attach function */
 }
 MODULE_DEF;
 
