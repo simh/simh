a2f7fef078b14289ebbf0ab8c58740a75c05aa83
Anton Kochkov <anton.kochkov@gmail.com>
2021-11-26 19:32:05 +0800

    Remove now unnecessary files

--- build_mingw_ether.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem Built in Ethernet support (requires WinPcap installed).
-rem The normal Windows build always builds with Ethernet support
-rem so, this procedure is un-necessary.  Just call the normal build
-rem
-%~p0\build_mingw.bat %*
